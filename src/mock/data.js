b82a0f3eaaa858d7174ada4c742822fe22b000c1
Lorenzo Levy <lorenzolevy@Lorenzos-MacBook-Pro.local>
2020-10-24 17:16:46 -0700

    Added updated portfolio, added new project section for 100k

--- src/mock/data.js
+++ src/mock/data.js
@@ -18,20 +18,31 @@ export const heroData = {
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: 'My journey as a developer began as UX researcher struggling to use python to arrange images and use SDKs so robots would behave properly in studies. I found love for finally solving that problem, or cleaning a console of errors.',
-  paragraphTwo: 'After graduating, I knew I wanted to be involved in building products rather than researching interactions with them, so I began making products for individuals and businesses. Below are some projects that I am proud of.',
+  paragraphOne:
+    'My journey as a developer began as UX researcher struggling to use python to arrange images and use SDKs so robots would behave properly in studies. I found love for finally solving that problem, or cleaning a console of errors.',
+  paragraphTwo:
+    'After graduating, I knew I wanted to be involved in building products rather than researching interactions with them, so I began making products for individuals and businesses. Below are some projects that I am proud of.',
   paragraphThree: `When I'm not strapped into my chair with extra lumbar support, I enjoy teaching my cat new tricks and attempting to make songs in abelton with my partner.`,
-  resume: './LL_resumeV10.pdf', // if no resume, the button will not show up
+  resume: './LorenzoLevy_Resume_10-24-2020.pdf', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: uuidv4(),
+    img: '100kmentors.png',
+    title: 'ProMazo - 100k Mentors Marketing',
+    info: 'Tech: React, AWS Amplify, GraphQL, GatsbyJS, ZenHub, Styled Components',
+    info2: `I was approached by ProMazo's head recruiter to create a series of Gatsby sites to promote the companies initiatives. I used themeing and Styled Components to create a library of reusable React components. Alongside the UI designer and CEO, I was able to implement the components in rapidly outputting stunning marketing sites, one of which boasted a hefty 50% CTR (Click Through Ratio).`,
+    url: 'https://mentors.promazo.com/',
+  },
+  {
+    id: uuidv4(),
     img: 'selah.png',
     title: 'Selah Roofing Corp',
     info: 'Tech: WordPress, ACF, GraphQL, GatsbyJS, Netlify',
-    info2: 'The owner of Selah Roofing Corp Calvin Black called me asking if I could bring his website into the 21 century. I was able to deliver a responsive website with perfect lighthouse scores, while maintaining his ability to both post articles and projects to maintain SEO "juice" using a headless WordPress setup.',
+    info2:
+      'The owner of Selah Roofing Corp Calvin Black called me asking if I could bring his website into the 21 century. I was able to deliver a responsive website with perfect lighthouse scores, while maintaining his ability to both post articles and projects to maintain SEO "juice" using a headless WordPress setup.',
     url: 'https://www.selahroofingcorp.com/',
     repo: 'https://github.com/lorenzolevy/selah-roofing', // if no repo, the button will not show up
   },
@@ -40,7 +51,8 @@ export const projectsData = [
     img: 'alisene.png',
     title: 'Ali Sene Art Portfolio',
     info: 'Tech: GatsbyJS, SASS modules, GraphQL',
-    info2: 'Working with Ali Sene was a wonderful opportunity to use JAM stack with a stakeholder to take advantage of the speed, security, and potential SEO enhancements offerred by Gatsby. Ali and I worked together to create an online art portfolio, which resulted in an increase in her bids and eventual employment as a graphic designer.',
+    info2:
+      'Working with Ali Sene was a wonderful opportunity to use JAM stack with a stakeholder to take advantage of the speed, security, and potential SEO enhancements offerred by Gatsby. Ali and I worked together to create an online art portfolio, which resulted in an increase in her bids and eventual employment as a graphic designer.',
     url: 'https://aliseneart.live/',
     repo: 'https://github.com/lorenzolevy/ali-sene', // if no repo, the button will not show up
   },
